c6e506542840d0abefcb845393e280842130afac
Ragnar Hrafnkelsson <ragnar.hrafnkelsson@gmail.com>
2013-01-20 00:54:18 +0000

    Added openFrameworks project

---
+++ ofMaximExample007OSX2/src/testApp.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxMaxim.h"
+
+
+class testApp : public ofBaseApp{
+
+	public:
+		~testApp();/* destructor is very useful */
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed  (int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+	
+		void audioRequested 	(float * input, int bufferSize, int nChannels); /* output method */
+		void audioReceived 	(float * input, int bufferSize, int nChannels); /* input method */
+	
+		int		initialBufferSize; /* buffer size */
+		int		sampleRate;
+	
+	
+		/* stick you maximilian stuff below */
+	
+		double leftOutput, rightOutput ,outputs[2];
+        double outVolume;
+		ofxMaxiMix mymix;
+        maxiEnv env;
+		ofxMaxiSample sample;
+    
+        int sampleNumber;
+        bool playSample;
+        vector<ofFile>samplePaths;
+	
+		
+};
