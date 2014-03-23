27d7bc8444e17cf05024be544259b824ece79d60
Owen Hindley <owenhindley@hotmail.com>
2014-03-23 14:48:59 +0000

    fixed indentation bollocks

--- StockholmCreatorKivy/deploy/app.py
+++ StockholmCreatorKivy/deploy/app.py
@@ -28,26 +28,6 @@ class LoadDialog(Popup):
 
 class MyApp(App):
 
-    def build(self):
-
-    	self.layout = FloatLayout()
-
-    	self.audioAnalyser = None
-
-    	self.selectSourceButton = Button(text="Select Source Folder", font_size=14, size_hint=(0.2, 0.2), pos_hint={'x' : .2, 'y' : .2})
-    	self.selectSourceButton.bind(on_press=self.onSelectSourceFolderClick)
-
-    	self.selectDestinationButton = Button(text="Select Destiation Folder", font_size=14, size_hint=(0.2, 0.2), pos_hint={'x' : .2, 'y' : .6})
-    	self.selectDestinationButton.bind(on_press=self.onSelectDestinationFolderClick)
-
-
-
-    	self.layout.add_widget(self.selectSourceButton)
-    	self.layout.add_widget(self.selectDestinationButton)
-
-    	# self.layout.add_widget(self.createFileBrowser())
-    
-        return self.layout
 
 
 	def sourceFolderSelectCallback(self):
@@ -59,31 +39,51 @@ class MyApp(App):
 		print("Opened filebrowser successfully")
 	
 
-    def onSelectSourceFolderClick(self, instance):
-
-    	user_path = expanduser('~') + sep + 'Documents'
-        self.fileBrowser = FileBrowser(select_string='Select', favorites=[(user_path, 'Documents')])
-        self.fileBrowser.bind(on_success=self.sourceFolderSelectCallback,on_canceled=self.fBrowserCancelled)
-        self.layout.add_widget(self.fileBrowser)
-
-    def onSelectDestinationFolderClick(self, instance):
+	def onSelectSourceFolderClick(self, instance):
 
 		user_path = expanduser('~') + sep + 'Documents'
 		self.fileBrowser = FileBrowser(select_string='Select', favorites=[(user_path, 'Documents')])
-		self.fileBrowser.bind(on_success=self.destFolderSelectCallback,on_canceled=self.fBrowserCancelled)
+		self.fileBrowser.bind(on_success=self.sourceFolderSelectCallback)
+		self.layout.add_widget(self.fileBrowser)
+
+	def onSelectDestinationFolderClick(self, instance):
+
+		user_path = expanduser('~') + sep + 'Documents'
+		self.fileBrowser = FileBrowser(select_string='Select', favorites=[(user_path, 'Documents')])
+		self.fileBrowser.bind(on_success=self.destFolderSelectCallback)
 		
 		self.layout.add_widget(self.fileBrowser)
 
 
-    def doAnalysis(self, instance):
+	def doAnalysis(self, instance):
 
 		self.analyser = StockholmAudioAnalyser("./testSamples", "./outputSamples", 0.2, 0.6, 1)
 		self.analyser.analyseFiles()
 		self.analyser.sortSimilarSamples()
 		self.analyser.exportFiles()
 
-	def fBrowserCancelled(self, instance):
-		print("browser cancelled")
+
+	def build(self):
+
+		self.layout = FloatLayout()
+
+		self.audioAnalyser = None
+
+		self.selectSourceButton = Button(text="Select Source Folder", font_size=14, size_hint=(0.2, 0.2), pos_hint={'x' : .2, 'y' : .2})
+		self.selectSourceButton.bind(on_press=self.onSelectSourceFolderClick)
+
+		self.selectDestinationButton = Button(text="Select Destiation Folder", font_size=14, size_hint=(0.2, 0.2), pos_hint={'x' : .2, 'y' : .6})
+		self.selectDestinationButton.bind(on_press=self.onSelectDestinationFolderClick)
+
+
+
+		self.layout.add_widget(self.selectSourceButton)
+		self.layout.add_widget(self.selectDestinationButton)
+
+		# self.layout.add_widget(self.createFileBrowser())
+
+		return self.layout
 
 if __name__ == '__main__':
+	MyApp().run()
     
